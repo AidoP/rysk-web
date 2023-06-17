bd032397071a29b150ba8fca7309665e015e99b6
Aidan Prangnell <aidan@trifuse.xyz>
2023-06-17 19:00:55 +0800

    fix type vue-tsc not finding dependencies with wrapper script

--- vite.config.mts
+++ vite.config.mts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'npm:vite@^4.3.9'
 import vue from 'npm:@vitejs/plugin-vue@^4.2.3'
-
 import 'npm:vue@^3.3.4'
 import wasm from 'npm:vite-plugin-wasm@^3.2.2'
 
---
+++ vue-tsc.ts
@@ -0,0 +1,9 @@
+import 'npm:@vitejs/plugin-vue@^4.2.3';
+import 'npm:typescript@^5.0.2';
+import 'npm:@babel/types';
+import 'npm:vite@^4.3.9';
+import 'npm:vue@^3.3.4';
+import 'npm:@vitejs/plugin-vue@^4.2.3';
+import 'npm:vite-plugin-wasm@^3.2.2';
+
+import 'npm:vue-tsc@^1.6.5/bin/vue-tsc.js';
